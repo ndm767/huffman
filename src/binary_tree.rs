64e78f9b743da8e47e9e58a5dfe8662135c20f04
ndm767 <5031736+ndm767@users.noreply.github.com>
2022-10-13 16:31:16 -0700

    did huffman encoding

---
+++ src/binary_tree.rs
@@ -0,0 +1,55 @@
+#[derive(Clone)]
+pub struct BinaryNode {
+    symbol: Option<char>,
+    prob: f32,
+    left: Option<Box<BinaryNode>>,
+    right: Option<Box<BinaryNode>>,
+}
+
+impl BinaryNode {
+    pub fn new(
+        prob: f32,
+        symbol: Option<char>,
+        left: Option<Box<BinaryNode>>,
+        right: Option<Box<BinaryNode>>,
+    ) -> BinaryNode {
+        BinaryNode {
+            symbol,
+            prob,
+            left,
+            right,
+        }
+    }
+
+    pub fn get_symbol(&self) -> Option<char> {
+        self.symbol
+    }
+    pub fn get_prob(&self) -> f32 {
+        self.prob
+    }
+
+    pub fn get_hamming_code(&self) -> Vec<(char, String)> {
+        let mut ret: Vec<(char, String)> = vec![];
+        let has_children = self.right.is_some() && self.left.is_some();
+        if has_children {
+            let mut right_ret = self.right.as_ref().unwrap().get_hamming_code();
+            for r in right_ret.iter_mut() {
+                r.1 = format!("1{}", r.1);
+            }
+            ret.append(&mut right_ret);
+
+            let mut left_ret = self.left.as_ref().unwrap().get_hamming_code();
+            for l in left_ret.iter_mut() {
+                l.1 = format!("0{}", l.1);
+            }
+            ret.append(&mut left_ret);
+        } else {
+            match &self.symbol {
+                Some(c) => ret.push((*c, String::from(""))),
+                None => panic!("Node has no children or symbol!"),
+            }
+        }
+
+        return ret;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+extern crate csv;
+
+pub mod binary_tree;
+
+use binary_tree::BinaryNode;
+use std::env;
+
+fn main() {
+    let mut argv = env::args();
+    if argv.len() <= 1 {
+        println!("No CSV file provided!");
+        return;
+    }
+
+    let mut alphabet: Vec<(char, f32)> = vec![];
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(argv.nth(1).unwrap())
+        .unwrap();
+    for res in rdr.records() {
+        let rec = res.unwrap();
+        let symbol = rec.get(0).unwrap().chars().nth(0).unwrap();
+        let prob = rec.get(1).unwrap().trim().parse::<f32>().unwrap();
+        alphabet.push((symbol, prob));
+    }
+
+    alphabet.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+    let mut tree: Vec<BinaryNode> = vec![];
+    for a in alphabet.iter() {
+        tree.push(BinaryNode::new(a.1, Some(a.0), None, None));
+    }
+
+    while tree.len() > 1 {
+        let mut i = 0usize;
+        let start_len = tree.len();
+        let mut new_tree: Vec<BinaryNode> = vec![];
+        while i < start_len {
+            println!("{} {} {}", i, start_len, tree.len());
+            if i < start_len - 1 {
+                let l = &tree[i];
+                let r = &tree[i + 1];
+                new_tree.push(BinaryNode::new(
+                    l.get_prob() + r.get_prob(),
+                    None,
+                    Some(Box::new(l.clone())),
+                    Some(Box::new(r.clone())),
+                ))
+            } else {
+                new_tree.push(tree[i].clone());
+            }
+            i += 2;
+        }
+
+        tree = new_tree.clone();
+    }
+
+    println!("Alphabet {:?}", alphabet);
+    println!("Code {:?}", tree[0].get_hamming_code());
+}
