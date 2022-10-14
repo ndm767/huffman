9abe4ff3047712a1d79da58953d1ccdc774da990
ndm767 <5031736+ndm767@users.noreply.github.com>
2022-10-13 21:41:50 -0700

    made the encoder more space-efficient

--- src/main.rs
+++ src/main.rs
@@ -74,17 +74,21 @@ fn main() {
     if encode {
         // create lookup table
         let mut max_symb: usize = 0;
+        let mut min_symb: usize = huffman_code[0].0 as usize;
         for i in 0..huffman_code.len() {
             if huffman_code[i].0 as usize > max_symb {
                 max_symb = huffman_code[i].0 as usize;
             }
+            if (huffman_code[i].0 as usize) < min_symb {
+                min_symb = huffman_code[i].0 as usize;
+            }
         }
         let mut lookup: Vec<String> = Vec::new();
-        for _i in 0..(max_symb as usize + 2) {
+        for _i in 0..(max_symb - min_symb + 2) {
             lookup.push(String::from(""));
         }
         for i in 0..huffman_code.len() {
-            lookup[huffman_code[i].0 as usize] = huffman_code[i].1.clone();
+            lookup[huffman_code[i].0 as usize - min_symb] = huffman_code[i].1.clone();
         }
 
         println!("Original message:\n{}", in_file);
@@ -92,7 +96,7 @@ fn main() {
         for c in in_file.chars() {
             // hack for control characters
             if !(c == '\n' || c == ' ') {
-                print!("{}", lookup[c as usize]);
+                print!("{}", lookup[c as usize - min_symb]);
             } else {
                 print!("{}", c);
             }
